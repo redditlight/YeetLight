8cdb6d5dc2ef42c3ae43f660561ca8d7cb9203b6
SamZyskowski <zyskowskisam@gmail.com>
2019-03-23 08:50:47 -1000

    questionable semantic integration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,56 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import 'semantic-ui-css/semantic.min.css';
+import React from 'react';
 import * as serviceWorker from './serviceWorker';
+import { Container, Divider, Grid, Menu, Image, List } from 'semantic-ui-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TopMenu extends React.Component{
+
+  state = {};
+
+  handleItemClick = (e, {name}) => this.setState({activeItem: name});
+
+  render() {
+    const itemStyle = { fontFamily: 'monospace', fontSize: '20px', color: 'black'};
+    const { activeItem } = this.state;
+    return (
+        <Menu>
+          <Menu.Item header>YeeLight</Menu.Item>
+          <Menu.Item
+              name='aboutUs'
+              active={activeItem === 'aboutUs'}
+              onClick={this.handleItemClick}
+          />
+          <Menu.Item name='brightness' active={activeItem === 'brightness'} onClick={this.handleItemClick} />
+          <Menu.Item
+              name='activity'
+              active={activeItem === 'activity'}
+              onClick={this.handleItemClick}
+          />
+        </Menu>
+    )
+  }
+
+}
+
+class YeeLight extends React.Component{
+
+  render(){
+    return(
+        <div>
+          <TopMenu/>
+        </div>
+    );
+  }
+}
+
+
+
+
+
+ReactDOM.render(<YeeLight/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
