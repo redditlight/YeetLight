e6be68c7df3596a40c6be10932a9a9ba6090fc39
Unknown <jpham79@hawaii.edu>
2019-04-10 23:09:00 -1000

    Added Echarts and changed data structure for "karma" post request

---
+++ frontend/src/components/KarmaChart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+class KarmaChart extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+
+      };
+
+      // This binding is necessary to make `this` work in the callback
+
+    }
+  
+  
+  
+  
+    render(){  
+  
+      return(
+        <p></p>
+      );
+    }
+  }
+  
+  export default KarmaChart;
